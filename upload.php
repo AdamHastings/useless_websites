557ea8a0307c473c1c0e93af01129e39f319165d
adamhastings <itsadamhastings@gmail.com>
2021-05-07 12:17:10 -0400

    pdfs only. clean house

--- upload.php
+++ upload.php
@@ -1,54 +1,31 @@
 <?php
-error_reporting(E_ALL);
-
-$target_dir = "./";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-echo $target_file;
-
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+if ($_POST["label"]) {
+    $label = $_POST["label"];
 }
+$allowedExts = array("pdf");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "application/pdf"))
+&& ($_FILES["file"]["size"] < 50000000)
+&& in_array($extension, $allowedExts)) {
+    if ($_FILES["file"]["error"] > 0) {
+        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    } else {
+        $filename = $label.$_FILES["file"]["name"];
+        echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+        echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+        if (file_exists("uploads/" . $filename)) {
+            echo $filename . " already exists. ";
+        } else {
+            move_uploaded_file($_FILES["file"]["tmp_name"],
+            "uploads/" . $filename);
+            echo "Stored in: " . "uploads/" . $filename;
+        }
+    }
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+    echo "Invalid file";
 }
 ?>

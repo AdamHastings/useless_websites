4423680f47e00e65ab051530bcd93ca2f90da5a4
adamhastings <itsadamhastings@gmail.com>
2021-05-07 11:57:06 -0400

    upload2 works?

--- upload.php
+++ upload.php
@@ -1,8 +1,13 @@
 <?php
-$target_dir = "uploads/";
+error_reporting(E_ALL);
+
+$target_dir = "./";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+echo $target_file;
+
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
